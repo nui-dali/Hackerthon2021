3b39d696d623b9ae0f22eddf41c34a032a4c1973
Jiyun Yang <ji.yang@samsung.com>
2021-07-26 10:52:59 +0900

    [Tutorial] Update button tutorial
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

---
+++ Tutorial/Components/99_Buttons_portrait/src/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace ButtonsTutorial
+{
+  public partial class MainView : View
+  {
+    public MainView() : base()
+    {
+      InitializeComponent();
+      Radio1.ItemGroup.SelectedChanged += OnRadioGroupChanged;
+    }
+
+    void OnButtonClicked(object sender, EventArgs args)
+    {
+      OutputText.Text += $"\nbutton clicked!";
+    }
+
+    void OnCheckBoxChanged(object sender, EventArgs args)
+    {
+      if (sender is CheckBox checkBox)
+      {
+        string state = checkBox.IsSelected ? "checked" : "unchecked";
+        OutputText.Text += $"\n{checkBox.Text} is {state}!";   
+      }
+    }
+
+    void OnRadioGroupChanged(object sender, EventArgs args)
+    {
+      if (sender is RadioButtonGroup group)
+      {
+        OutputText.Text += $"\n{group.GetSelectedItem().Text} is selected!";
+      }
+    }
+
+    void OnSwitchChanged(object sender, EventArgs args)
+    {
+      if (sender is Switch switchButton)
+      {
+        string state = switchButton.IsSelected ? "on" : "off";
+        OutputText.Text += $"\n{switchButton.Text} is {state}!";
+      }
+    }
+  }
+}
