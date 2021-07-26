111021371964a4602eb6e8539e99e6d36ddbc11e
Seoyeon Kim <seoyeon2.kim@samsung.com>
2021-07-26 20:09:41 +0900

    [Tutorial] Add Menu tutorial sample
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

---
+++ Tutorial/Components/4_Menu/MenuTutorial/Scene1Page.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace MenuTutorial
+{
+    public partial class Scene1Page : View
+    {
+        MenuItem menuItem1;
+        MenuItem menuItem2;
+        MenuItem menuItem3;
+        MenuItem menuItem4;
+        MenuItem menuItem5;
+
+        public Scene1Page()
+        {
+            InitializeComponent();
+
+            // TODO : Make more simple
+            menuItem1 = new MenuItem();
+            menuItem1.Text = "Menu1";
+            menuItem1.Clicked += OnItemClicked;
+
+            menuItem2 = new MenuItem();
+            menuItem2.Text = "Menu2";
+            menuItem2.Clicked += OnItemClicked;
+
+            menuItem3 = new MenuItem();
+            menuItem3.Text = "Menu3";
+            menuItem3.Clicked += OnItemClicked;
+
+            menuItem4 = new MenuItem();
+            menuItem4.Text = "Menu4";
+            menuItem4.Clicked += OnItemClicked;
+
+            menuItem5 = new MenuItem();
+            menuItem5.Text = "Menu5";
+            menuItem5.Clicked += OnItemClicked;
+        }
+
+        private void OnClicked(object sender, Tizen.NUI.Components.ClickedEventArgs e)
+        {
+            var menu = new Menu()
+            {
+                Anchor = moreButton,
+                HorizontalPositionToAnchor = Menu.RelativePosition.Center,
+                VerticalPositionToAnchor = Menu.RelativePosition.End,
+                Items = new MenuItem[] { menuItem1, menuItem2, menuItem3, menuItem4, menuItem5 }
+            };
+
+            menu.Post();
+        }
+
+        private void OnItemClicked(object sender, Tizen.NUI.Components.ClickedEventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                OutputText.Text += $"\n{menuItem.Text} is clicked!";
+            }
+        }
+    }
+}
---
+++ Tutorial/Components/4_Menu/README.md
@@ -0,0 +1,3 @@
+# Menu tutorial
+
+<div style="text-align:center;width:100%;"><img src="./MenuTutorial/res/screenshot.png" /></div>
