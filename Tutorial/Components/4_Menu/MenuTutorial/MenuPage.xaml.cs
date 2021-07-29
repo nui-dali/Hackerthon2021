4a8465847d94c57a54b7314419cc18b2c00e65c0
Seoyeon Kim <seoyeon2.kim@samsung.com>
2021-07-29 17:11:54 +0900

    [Tutorial] Update Menu sample
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

---
+++ Tutorial/Components/4_Menu/MenuTutorial/MenuPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace MenuTutorial
+{
+    public partial class MenuPage : Menu
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            // TODO : There might be an issue on Clicked event of MenuItem when using it in xaml.
+            menuItem1.Clicked += OnItemClicked;
+            menuItem2.Clicked += OnItemClicked;
+            menuItem3.Clicked += OnItemClicked;
+            menuItem4.Clicked += OnItemClicked;
+            menuItem5.Clicked += OnItemClicked;
+        }
+
+        private void OnItemClicked(object sender, ClickedEventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                Scene1Page page = new Scene1Page();
+                page.OutputText.Text += $"\n{menuItem.Text} is clicked!";
+            }
+        }
+    }
+}
--- Tutorial/Components/4_Menu/MenuTutorial/Scene1Page.xaml.cs
+++ Tutorial/Components/4_Menu/MenuTutorial/Scene1Page.xaml.cs
@@ -6,57 +6,23 @@ namespace MenuTutorial
 {
     public partial class Scene1Page : View
     {
-        MenuItem menuItem1;
-        MenuItem menuItem2;
-        MenuItem menuItem3;
-        MenuItem menuItem4;
-        MenuItem menuItem5;
+        private MenuPage menu;
 
         public Scene1Page()
         {
             InitializeComponent();
-
-            // TODO : Make more simple
-            menuItem1 = new MenuItem();
-            menuItem1.Text = "Menu1";
-            menuItem1.Clicked += OnItemClicked;
-
-            menuItem2 = new MenuItem();
-            menuItem2.Text = "Menu2";
-            menuItem2.Clicked += OnItemClicked;
-
-            menuItem3 = new MenuItem();
-            menuItem3.Text = "Menu3";
-            menuItem3.Clicked += OnItemClicked;
-
-            menuItem4 = new MenuItem();
-            menuItem4.Text = "Menu4";
-            menuItem4.Clicked += OnItemClicked;
-
-            menuItem5 = new MenuItem();
-            menuItem5.Text = "Menu5";
-            menuItem5.Clicked += OnItemClicked;
         }
 
         private void OnClicked(object sender, Tizen.NUI.Components.ClickedEventArgs e)
         {
-            var menu = new Menu()
+            menu = new MenuPage()
             {
                 Anchor = moreButton,
                 HorizontalPositionToAnchor = Menu.RelativePosition.Center,
-                VerticalPositionToAnchor = Menu.RelativePosition.End,
-                Items = new MenuItem[] { menuItem1, menuItem2, menuItem3, menuItem4, menuItem5 }
+                VerticalPositionToAnchor = Menu.RelativePosition.End
             };
 
             menu.Post();
-        }
-
-        private void OnItemClicked(object sender, Tizen.NUI.Components.ClickedEventArgs e)
-        {
-            if (sender is MenuItem menuItem)
-            {
-                OutputText.Text += $"\n{menuItem.Text} is clicked!";
-            }
         }
     }
 }
