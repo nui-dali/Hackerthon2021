6956a5cf19a583bfca6055b7d9b79efafbcb8dfa
Jaehyun Cho <jae_hyun.cho@samsung.com>
2021-08-03 22:01:17 +0900

    [Tutorial] Add and Update Layout tutorials
    AbsoluteLayout tutorial has been added.
    RelativeLayout tutorial has been added.
    LinearLayout tutorial has been updated.

---
+++ Tutorial/Layouts/AbsoluteLayout/Scene1Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace AbsoluteLayout
+{
+    public partial class Scene1Page : View
+    {
+        public Scene1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tutorial/Layouts/RelativeLayout/Scene1Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace RelativeLayout
+{
+    public partial class Scene1Page : View
+    {
+        public Scene1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
